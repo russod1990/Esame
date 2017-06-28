713b7676818959be367e82b2cb0f66cea46848b8
tss <tss@debian-tss24>
2017-06-28 15:47:38 +0200

    EsameApp avanzamento Asta

--- src/main/java/EsameApp/Resources/AstaResources.java
+++ src/main/java/EsameApp/Resources/AstaResources.java
@@ -5,8 +5,19 @@
  */
 package EsameApp.Resources;
 
+import EsameApp.Entity.Asta;
+import EsameApp.Services.AstaManager;
+import EsameApp.Services.TokenManager;
+import EsameApp.Services.TokenNeeded;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -18,5 +29,59 @@ import javax.ws.rs.core.MediaType;
 @Path("Aste")
 @Produces({MediaType.APPLICATION_JSON})
 public class AstaResources {
-    
+
+    @Inject
+    TokenManager tokenManager;
+
+    @Inject
+    AstaManager astaManager;
+
+    @GET
+    public List<Asta> findAll() {
+        return astaManager.findAll();
+    }
+
+    @GET
+    @Path("mieAste")
+    @TokenNeeded
+    public List<Asta> findByUser() {
+        long id = tokenManager.getCurrentUser().getIdUtente();
+        return astaManager.findByUser(id);
+    }
+
+    @GET
+    @Path("Aste")
+    @TokenNeeded
+    public List<Asta> findByOtherUser() {
+        long id = tokenManager.getCurrentUser().getIdUtente();
+        return astaManager.findByOtherUser(id);
+    }
+
+    @POST
+    @Path("nuova")
+    @TokenNeeded
+    public void crea(Asta a) {
+        System.out.println("******* nuova asta ********");
+        a.setUtente(tokenManager.getCurrentUser());
+        astaManager.save(a);
+    }
+
+    @DELETE
+    @Path("{id}")
+    @TokenNeeded
+    public void delete(@PathParam("id") long id) {
+        System.out.println("******* delete ************");
+        astaManager.remove(id);
+    }
+
+    @PUT
+    @Path("{id}")
+    @TokenNeeded
+    public void update(@PathParam("id") long id, Asta a) {
+        System.out.println("******* update **********");
+        a.setUtente(tokenManager.getCurrentUser());
+        a.setIdAsta(id);
+        astaManager.save(a);
+//        }
+    }
 }
